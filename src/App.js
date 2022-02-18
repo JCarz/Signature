c75c7bcd4a8c3d5e165c1376a77562fa399cd995
Jaylen Carroll <jaylencarroll87@gmail.com>
2022-02-17 19:27:49 -0500

    Added State to the form submit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [showForm, setShowForm] = useState(false)
+  const showForm1 = () => {
+    setShowForm(!showForm)
+  }
   return (
     <div className="wrapper">
       <h1>Form Submit</h1>
-      <form>
-        <fieldset>
-          <label>
-            <p>Complete Full Name</p>
-            <input name="name" placeholder="Type Here" />
-            <p>Title</p>
-            <input title="tile" placeholder="Type Here" />
-            <p>Company (optional)</p>
-            <input company="company" placeholder="Type Here" />
-          </label>
-          <hr></hr>
 
-          <p>
-            {" "}
-            <img
-              name="timer"
-              src="https://img.icons8.com/fluency-systems-regular/48/000000/timer.png"
-            />
-            Waiting for <strong>Persons</strong> signature
-          </p>
-        </fieldset>
-        <button type="submit">Submit</button>
-      </form>
+      <button className="signHere" onClick={showForm1}>
+        Sign Here
+      </button>
+
+      {showForm && (
+        <form>
+          <div className="signaturePic"></div>
+          <fieldset>
+            <label>
+              <p>Complete Full Name</p>
+              <input name="name" placeholder="Type Here" />
+              <p>Title</p>
+              <input title="tile" placeholder="Type Here" />
+              <p>Company (optional)</p>
+              <input company="company" placeholder="Type Here" />
+            </label>
+            <hr></hr>
+
+            <p>
+              {" "}
+              <img
+                name="timer"
+                src="https://img.icons8.com/fluency-systems-regular/48/000000/timer.png"
+              />
+              Waiting for <strong>Persons</strong> signature
+            </p>
+          </fieldset>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   )
 }
